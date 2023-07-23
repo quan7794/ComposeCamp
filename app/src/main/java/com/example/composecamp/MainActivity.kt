84f95ff69717ff1d728ed5bb89ea4f0eed318f82
quan-home <quanmtfu@gmail.com>
2023-07-24 00:25:34 +0700

    44457

--- app/src/main/java/com/example/composecamp/MainActivity.kt
+++ app/src/main/java/com/example/composecamp/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.composecamp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composecamp.ui.theme.ComposeCampTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +31,7 @@ class MainActivity : ComponentActivity() {
             ComposeCampTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    GreetingImage("Android", "From " + "Quan")
                 }
             }
         }
@@ -29,11 +39,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Blue) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    // Create a column so that texts don't overlap
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
         Text(
-            text = "Hello $name!",
-            modifier = modifier
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
@@ -42,6 +65,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeCampTheme {
-        Greeting("Android")
+        GreetingText("Sam", "From Quan")
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_launcher_background)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = "Happy Birthday",
+            modifier = modifier,
+            contentScale = ContentScale.FillBounds,
+            alpha = 0.1f
+        )
+        GreetingText(
+            message, from, modifier = modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
